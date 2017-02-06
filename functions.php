6f524e7214bb7e31d78f8d585e8c0bb1d73517f9
webmestreallesi <webmestre@allesi.ca>
2017-02-06 13:26:58 -0500

    add
    add

---
+++ functions.php
@@ -0,0 +1,62 @@
+<?php
+// Exit if accessed directly
+if ( !defined( 'ABSPATH' ) ) exit;
+
+// BEGIN ENQUEUE PARENT ACTION
+// AUTO GENERATED - Do not modify or remove comment markers above or below:
+         
+if ( !function_exists( 'child_theme_configurator_css' ) ):
+    function child_theme_configurator_css() {
+        wp_enqueue_style( 'chld_thm_cfg_child', trailingslashit( get_stylesheet_directory_uri() ) . 'style.css', array( 'slick_css','theguard_alicobold','theguard_sicon','theguard_fontawesome','theguard_css','theguard_ownstyles','theguard_ownstyles' ) );
+    }
+endif;
+add_action( 'wp_enqueue_scripts', 'child_theme_configurator_css' );
+
+// END ENQUEUE PARENT ACTION
+
+/**** DEBUT TOP BAR CUSTOMIZATION***/
+// Topbar 2 customization :
+//	- align mail/phobe left
+// 	- add polylang switcher languages
+//doc : https://polylang.wordpress.com/documentation/documentation-for-developers/functions-reference/
+if ( ! function_exists( 'theguard_topbar_r' ) ) {
+	function theguard_topbar_r()
+	{
+		global $secretlab;
+		if (isset ($secretlab['header-topbar'])) {
+			$sl_header_topbar = $secretlab['header-topbar'];
+			if ($sl_header_topbar == 1) {
+				echo '<div class="sell_topbar">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-6 col-sm-12 col-xs-12 cdata">';
+					theguard_phone_header();
+					//theguard_email_header();
+				echo '</div>
+				<div class="col-md-2 col-sm-12 col-xs-12 languages">';
+					//call language switcher polylang
+					echo '<ul>';
+					pll_the_languages(array('show_flags'=>1,'show_names'=>1, 'hide_current'=>1, 'dropdown'=>0));
+					echo '</ul>';
+				echo '</div>
+				<div class="col-md-2 col-sm-12 col-xs-12 languages">';
+					//lien espace client
+					echo '<ul>';
+					wp_nav_menu(array( 'menu' => 45, 'container_class' => 'menu-customer_area',
+    'echo' => true, 'before' => '<i class="ultimate1600-user"></i>', 'after' => '', 'link_before' => ''));
+					echo '</ul>';
+				echo '</div>  
+				<div class="col-md-2 col-sm-12 col-xs-12 social">';	
+					theguard_socialbuttons_header();
+				echo '</div>  
+				  					
+			</div>
+		</div>
+	</div>';
+			}
+		}
+	}
+}
+
+
+/*FIN TOP BAR CUSTOMIZATION*/
---
+++ header6.php
@@ -0,0 +1,56 @@
+<!-- HEADER 6 START -->
+<div class="shortheader1 <?php sell_header6_switch(); ?>">
+    <?php theguard_topbar_r(); ?>
+
+    <div class="sell_menuline default" <?php theguard_sticky_class(); ?>>
+        <div class="container">
+            <div class="row">
+                <div class="col-md-3 col-sm-4 col-xs-12">
+                    <?php theguard_logo_img(); ?>
+                </div>
+                <div class="col-md-9 col-sm-8 col-xs-12 nav-wrap">
+                    <div class="navbar navbar-default">
+
+                            <div class="navbar-header visible-sm visible-xs pull-right">
+                                <button type="button" class="navbar-toogle open-menu-list" data-toggle="collapse" data-target="#responsive-menu"><i class="fa fa-bars"></i></button>
+                            </div>
+                            <div class="collapse navbar-collapse pull-right" id="responsive-menu">
+                                <ul class="nav navbar-nav nav-list">
+                                    <?php theguard_set_nav(); ?>
+                                    <?php theguard_search_menu(); ?>
+                                    <?php theguard_cart_menu(); ?>
+                                </ul>
+                            </div>
+                        
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="container-fluid pageheading">
+        <div class="overslider">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+                        <h1><?php
+                            echo get_the_title();
+                            ?></h1>
+                        <div class="hidden-xs hidden-sm">
+                            <div class="breadcrumbs" typeof="BreadcrumbList" vocab="http://schema.org/">
+                                <?php if ( function_exists('yoast_breadcrumb') )
+                                {yoast_breadcrumb('','');} ?>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+        <div class="sliderh4under">
+            <?php theguard_set_customized_slider(); ?>
+        </div>
+    </div>
+
+</div>
+<!-- HEADER 6 END -->
