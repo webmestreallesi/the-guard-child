aded61875920bd4a6b8b9f847a9ac09ca3988c60
webmestreallesi <webmestre@allesi.ca>
2017-02-07 13:23:40 -0500

    Revert "kclass"
    This reverts commit 17aa192f16442af8c5a4732679f5fe8398e5553c.

--- functions.php
+++ functions.php
@@ -127,7 +127,7 @@ function child_page_nav(){
 	if(count($siblings) > 1 && !is_null($args))   
 	{
 		echo '<div class="sidebar-left-list-pages">';
-				'<ul>';
+				'<ul class="sidebar-left-list-pages">';
 			wp_list_pages($args);
 		echo '	 </ul>';
 		echo ' </div>';
