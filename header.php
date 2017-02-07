89e303588f96b9843ef749b9947f90ecb848e66c
webmestreallesi <webmestre@allesi.ca>
2017-02-07 11:33:06 -0500

    child_page_nav

--- header.php
+++ header.php
@@ -39,40 +39,4 @@ sell_set_globals();
 			<?php
 			theguard_set_header_sidebar_layout();
 			?>
-<?//GET CHILD PAGES IF THERE ARE ANY
-		$children = get_pages('child_of='.$post->ID);
-
-		//GET PARENT PAGE IF THERE IS ONE
-		$parent = $post->post_parent;
-
-		//DO WE HAVE SIBLINGS?
-		$siblings =  get_pages('child_of='.$parent);
-
-		if( count($children) != 0) {
-		   $args = array(
-			 'depth' => 1,
-			 'title_li' => '',
-			 'child_of' => $post->ID
-		   );
-
-		} elseif($parent != 0) {
-			$args = array(
-				 'depth' => 1,
-				 'title_li' => '',
-				 'exclude' => $post->ID,
-				 'child_of' => $parent
-			   );
-		}
-		//Show pages if this page has more than one sibling 
-		// and if it has children 
-		if(count($siblings) > 1 && !is_null($args))   
-		{
-?>
-		<div class="widget subpages">
-		<h3 class="widgettitle">Also in this Section</h3>
-			 <ul class="pages-list">
-			 <?php wp_list_pages($args);  ?>
-			 </ul>
-		 </div>
-		  <?php } ?>
 
