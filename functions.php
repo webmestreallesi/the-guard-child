a3f430b4c8339e050f66b51162759d7978b5c672
webmestreallesi <webmestre@allesi.ca>
2017-02-07 11:05:51 -0500

    d

--- functions.php
+++ functions.php
@@ -62,7 +62,6 @@ if ( ! function_exists( 'theguard_topbar_r' ) ) {
 
 /*Sidebar customization*/
 function theguard_set_header_sidebar_layout_custom() {
-	global $secretlab, $theguard_layout;
 	$sl_sidebar_layout = isset($theguard_layout[$secretlab['theguard_pagetype_prefix'] . 'sidebar-layout']) ? $theguard_layout[$secretlab['theguard_pagetype_prefix'] . 'sidebar-layout'] : 1;
 	if ($sl_sidebar_layout == 2 or $sl_sidebar_layout == 3) {
 		echo '<div class="col-lg-3 col-md-3 col-sm-4 col-xs-12 widget-area">defaut';
