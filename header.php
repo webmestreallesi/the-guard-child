3f6392c69701d3ed03b54a4ae8edc5d12a677104
webmestreallesi <webmestre@allesi.ca>
2017-02-07 12:30:21 -0500

    ss

--- header.php
+++ header.php
@@ -25,7 +25,6 @@ sell_set_globals();
 </head>
 <?
 echo  "post courant :".$post_ID;
-echo '$foo dans le contexte global : ' . $GLOBALS["post"] . "\n";
 get_defined_vars();
 ?>
 <body <?php body_class(); ?>>
