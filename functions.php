23cd8e9faa67fa18525974c9836c2aded3c71f78
webmestreallesi <webmestre@allesi.ca>
2017-02-07 12:20:18 -0500

    sss

--- functions.php
+++ functions.php
@@ -95,9 +95,9 @@ function theguard_set_header_sidebar_layout_custom() {
 }
 /*end Sidebar customization*/
 
-function child_page_nav($post){
+function child_page_nav(){
 	//GET CHILD PAGES IF THERE ARE ANY
-	echo "post courant :" $post;
+	echo "post courant :".$post;
 	$children = get_pages('child_of='.$post);
 	//GET PARENT PAGE IF THERE IS ONE
 	$parent = $post->post_parent;
