3b5a22f2d42378355716b1fc3b79040b9eb5f782
webmestreallesi <webmestre@allesi.ca>
2017-02-08 17:11:36 -0500

    remove tab 2

--- functions.php
+++ functions.php
@@ -135,15 +135,8 @@ function child_page_nav(){
 }
 
 /*woocmmerce desactivation des onglets*/
-/** Remove product tabs in WooCommerce **/
-
-function woo_remove_product_tab_sb($tabs) {
-
-    unset( $tabs['additional_information'] );  	// Remove the additional information tab
-
- 	return $tabs;
- 
+add_filter( 'woocommerce_product_tabs', 'wcs_woo_remove_reviews_tab', 98 );
+    function wcs_woo_remove_reviews_tab($tabs) {
+    unset($tabs['reviews']);
+    return $tabs;
 }
-add_filter( 'woocommerce_product_tabs', 'woo_remove_product_tab_sb', 98);
-unset( $tabs['description'] );      		// Remove the description tab
-unset( $tabs['reviews'] ); 					// Remove the reviews tab
