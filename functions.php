94273bccfce1bed87a12d32a36c0e4a32fbb1271
webmestreallesi <webmestre@allesi.ca>
2017-02-08 17:12:27 -0500

    removetab good

--- functions.php
+++ functions.php
@@ -138,5 +138,6 @@ function child_page_nav(){
 add_filter( 'woocommerce_product_tabs', 'wcs_woo_remove_reviews_tab', 98 );
     function wcs_woo_remove_reviews_tab($tabs) {
     unset($tabs['reviews']);
+	unset($tabs['description']);
     return $tabs;
 }
