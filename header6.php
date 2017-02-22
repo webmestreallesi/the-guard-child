804116c81c9b18945a3c056f4745d9d42ff33622
webmestreallesi <webmestre@allesi.ca>
2017-02-22 12:58:13 -0500

    ok

--- header6.php
+++ header6.php
@@ -7,7 +7,6 @@
             <div class="row">
                 <div class="col-md-3 col-sm-4 col-xs-12">
                     <?php theguard_logo_img(); ?>
-					<div class="buttonMenu">helloworld!</div>
 
                 </div>
                 <div class="col-md-9 col-sm-8 col-xs-12 nav-wrap">
