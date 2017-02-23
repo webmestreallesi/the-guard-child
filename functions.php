2b85aec475f6c6447c46610cc4e17eac39c22f2c
webmestreallesi <webmestre@allesi.ca>
2017-02-23 16:40:51 -0500

    class

--- functions.php
+++ functions.php
@@ -37,7 +37,7 @@ if ( ! function_exists( 'theguard_topbar_r' ) ) {
 				<div class="col-md-2 col-sm-12 col-xs-12 languages">';
 					echo '<ul>';
 						wp_nav_menu(array( 'menu' => 'Demande de soumission Top', 'menu_id'=> 'soumission-top', 'container_class' => 'menu-customer_area',
-						'echo' => true, 'before' => '', 'after' => '', 'link_before' => ''));
+						'echo' => true, 'before' => '<i class="ultimate1600-user"></i>', 'after' => '', 'link_before' => ''));
 						echo '</ul>';
 				echo '</div>
 				<div class="col-md-2 col-sm-12 col-xs-12 languages">';
