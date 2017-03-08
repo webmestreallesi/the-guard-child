9beec43cd41e42e2df7e477fd53b3b64d24c6bab
webmestreallesi <webmestre@allesi.ca>
2017-03-08 15:08:13 -0500

    page recherche lyaout

---
+++ search.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * The template for displaying Search Results pages
+ */
+
+get_template_part( 'header-no-sidebar'); ?>
+
+	<div id="primary" class="content-area">
+		<div id="content" class="site-content searchresult" role="main">
+
+		<?php if ( have_posts() ) : ?>
+
+			<header class="page-header">
+				<h1 class="page-title"><?php printf( esc_html__( 'Search Results for: %s', 'the-guard' ), get_search_query() ); ?></h1>
+			</header>
+
+			<?php /* The loop */ ?>
+			<?php while ( have_posts() ) : the_post(); ?>
+				<?php get_template_part( 'content' ); ?>
+			<?php endwhile; ?>
+
+			<?php
+			echo '<div class="blogpagination">'.paginate_links().'</div>';
+			?>
+
+		<?php else : ?>
+			<?php get_template_part( 'content', 'none' ); ?>
+		<?php endif; ?>
+
+		</div><!-- #content -->
+	</div><!-- #primary -->
+
+
+<?php get_footer(); ?>
