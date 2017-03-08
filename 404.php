a0ff470952582ece9955e8ad75536be6b49144d6
webmestreallesi <webmestre@allesi.ca>
2017-03-08 14:11:41 -0500

    ccc

---
+++ 404.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * The template for displaying 404 pages (Not Found)
+ */
+
+get_header_no_sidebar(); ?>
+
+	<div id="primary" class="content-area mb80 error404">
+		<div id="content" class="site-content" role="main">
+			<header class="page-header">
+				<h1 class="page-title"><?php esc_html_e( 'Désolé ! Page non trouvée !', 'the-guard' ); ?></h1>
+			</header>
+
+			<div class="page-wrapper">
+				<div class="page-content text-center">
+					<div class="e404">
+						<div class="block">
+							<div class="wrapper" data-anim="base wrapper">	
+								<span class="sicon-cctv-2"></span>
+								<div class="circle" data-anim="base left"></div>
+								<div class="circle" data-anim="base right"></div>
+							</div>
+							<div class="big">404</div>
+						</div>
+
+						<p><?php esc_html_e( 'Il semble que le contenu de cette page a été déplacé. Nous vous proposons de refaire une recherche :', 'the-guard' ); ?></p>
+						<?php get_search_form(); ?>
+					</div>
+			</div><!-- .page-content -->
+			</div><!-- .page-wrapper -->
+
+		</div><!-- #content -->
+	</div><!-- #primary -->
+
+<?php get_footer(); ?>
