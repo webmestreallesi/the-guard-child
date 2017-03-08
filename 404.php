5281b857af13eaadd1f514fb22df00c6bc593e93
webmestreallesi <webmestre@allesi.ca>
2017-03-08 14:24:50 -0500

    ss

--- 404.php
+++ 404.php
@@ -9,7 +9,6 @@ get_template_part( 'header-no-sidebar');
 	<div id="primary" class="content-area mb80 error404">
 		<div id="content" class="site-content" role="main">
 			<div class="page-wrapper">
-				<h1><?php esc_html_e( 'Désolé ! Page non trouvée !', 'the-guard' ); ?></h1>
 				<div class="page-content text-center">
 					
 					<div class="e404">
@@ -19,9 +18,10 @@ get_template_part( 'header-no-sidebar');
 								<div class="circle" data-anim="base left"></div>
 								<div class="circle" data-anim="base right"></div>
 							</div>
+							
+							<h1><?php esc_html_e( 'Désolé ! Page non trouvée !', 'the-guard' ); ?></h1>
 							<div class="big">404</div>
 						</div>
-
 						<p><?php esc_html_e( 'Il semble que le contenu de cette page a été déplacé. Nous vous proposons de refaire une recherche :', 'the-guard' ); ?></p>
 						<?php get_search_form(); ?>
 					</div>
